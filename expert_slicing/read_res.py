4b24f99cb2396184e9f0920983a67fee238e675b
Weinan Dai <daiwn21@mails.tsinghua.edu.cn>
2023-08-27 19:02:35 +0000

    更新了散点图

--- expert_slicing/read_res.py
+++ expert_slicing/read_res.py
@@ -12,6 +12,8 @@ for res_dict in res:
     x.append(res_dict['HIDDEN_DIM'])
     y.append(res_dict['ratio'])
 plt.scatter(x, y)
+plt.plot([0, 16500], [0.625, 0.625], color='red', linewidth=2.0, linestyle='--')
+plt.text(16500, 0.625, 'theoretical limit', ha='right', va='bottom', fontsize=10)
 plt.xlabel("HIDDEN_DIM")
 plt.ylabel("ratio")
 plt.show()
---
+++ expert_slicing/repeat.sh
@@ -0,0 +1,5 @@
+sh loop.sh 500 16500
+sh loop.sh 500 16500
+sh loop.sh 500 16500
+sh loop.sh 500 16500
+sh loop.sh 500 16500
