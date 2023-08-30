a8ff5676a3e20c478c777a2f58b49ff394e00888
Weinan Dai <daiwn21@mails.tsinghua.edu.cn>
2023-08-30 08:42:46 +0000

    更新图表

--- expert_slicing/plot.py
+++ expert_slicing/plot.py
@@ -36,6 +36,7 @@ def plot(w, e, tp):
     plt.text(16500, t, 'Theoretical Limit', ha='right', va='bottom', fontsize=10)
     plt.xlabel("HIDDEN_DIM")
     plt.ylabel("Ratio of Inference Time")
+    plt.title(f"B={B}, SQ={SQ}, E={e}, TP={tp}")
     plt.savefig(f"pics_B{B}_SQ{SQ}/pics"+s+"/scatter"+s+".png")
 
     plt.figure()
@@ -44,6 +45,7 @@ def plot(w, e, tp):
     p1 = plt.scatter(x, sliced_time)
     p2 = plt.scatter(x, unsliced_time)
     plt.legend([p1, p2], ["Sliced MoE", "Unsliced MoE"])
+    plt.title(f"B={B}, SQ={SQ}, E={e}, TP={tp}")
     plt.savefig(f"pics_B{B}_SQ{SQ}/pics"+s+"/sep_scatter"+s+".png")
     # 折线图
     plt.figure()
@@ -55,6 +57,7 @@ def plot(w, e, tp):
     plt.text(16500, t, 'Theoretical Limit', ha='right', va='bottom', fontsize=10)
     plt.xlabel("HIDDEN_DIM")
     plt.ylabel("Ratio of Inference Time")
+    plt.title(f"B={B}, SQ={SQ}, E={e}, TP={tp}")
     plt.savefig(f"pics_B{B}_SQ{SQ}/pics"+s+"/line"+s+".png")
 
     plt.figure()
@@ -63,6 +66,7 @@ def plot(w, e, tp):
     p1, =plt.plot(x, sliced_time)
     p2, =plt.plot(x, unsliced_time)
     plt.legend([p1, p2], ["Sliced MoE", "Unsliced MoE"])
+    plt.title(f"B={B}, SQ={SQ}, E={e}, TP={tp}")
     plt.savefig(f"pics_B{B}_SQ{SQ}/pics"+s+"/sep_line"+s+".png")
 
 def all_plot(w, e, tp):
@@ -105,7 +109,8 @@ def all_plot(w, e, tp):
     p1, = ax2.plot(x, sliced_time, color='blue')
     p2, = ax2.plot(x, unsliced_time, color='orange')
     plt.legend([p1, p2, p], ["Sliced MoE", "Unsliced MoE", "Ratio of Inference Time"])
-
+    # 添加标题： title
+    plt.title(f"B={B}, SQ={SQ}, E={e}, TP={tp}")
     plt.savefig(f"pics_B{B}_SQ{SQ}/pics"+s+"/all_line"+s+".png")
 
 print(f"Plotting B={B} SQ={SQ}")
@@ -113,6 +118,7 @@ args =[(2,2,2), (2,4,2), (4,4,2), (4,4,4)]
 for arg in args:
     try:
         all_plot(*arg)
+        plot(*arg)
     except Exception as e:
         print(f"Failed for {arg}: {e}")
         continue
